49d71dd5db82f1f1f108bd48b86f44b90b69956b
Richard Lee <richlee81192@gmail.com>
2020-01-09 20:18:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ def self.create(name, grade)
 end 
 
 def self.new_from_db(row)
-  student = self.new
+  student = self.new(name, grade, id = nil)
   student.id = row[0]
   student.name = row[1]
   student.grade = row[2]
